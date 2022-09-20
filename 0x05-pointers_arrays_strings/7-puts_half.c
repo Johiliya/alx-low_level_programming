5097ab28e9da15aa4372a9de2d6ccbc260f57d0b
Johiliya <iliyajohnbobai@gmail.com>
2022-09-20 12:09:15 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * puts_half - prints a string
- * @str: string to print
- *
- * Return: nothing
- */
+* puts_half - prints a string
+* @str: string to print
+*
+* Return: nothing
+*/
 
 void puts_half(char *str)
 {
