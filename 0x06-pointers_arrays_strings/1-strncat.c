cbc0cbdae514a3936c82fa9d42c423154c6ed45d
Johiliya <iliyajohnbobai@gmail.com>
2022-09-22 16:13:35 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,6 @@
  *@n:integer parameter to compare index to
  *Return: returns new concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 
@@ -14,9 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[index++])
 		dest_len++;
-	
+
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
-
 	return (dest);
 }
