2058c9f050c2b06d9827451197d3ee1b3669331a
Johiliya <iliyajohnbobai@gmail.com>
2022-09-26 16:03:09 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,18 @@
-int _putchar(char c);            
-char *_memset(char *S, char b, unsigned int n);              
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char strchr(char *S, char c);
-unsigned int _strspn( char *S, char accept);
-char *_strpbrk(char *S, char *accept);
-char *_strstr(char *haystack, char *needle);                
-void print_chessboard(char (*a)[8]);                         
-void print_diagsums(int *a, int size);                      
-void set_string(char **S, char *to);                         
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+
+#endif
 
 
