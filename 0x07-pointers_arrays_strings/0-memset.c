50c7cbfab23f2070e726cdcc9b896027e46d9365
Johiliya <iliyajohnbobai@gmail.com>
2022-09-26 14:28:32 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
+#include "main.h"
 /**
- * _memset - fills a memory block with a constant byte
- * @s: address to memory block
- * @b: char to be used
- * @n: number of bytes to be used
  *
- * Return: pointer to the memory block
+ * _memset - fills memory with a contant byte.
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: lenght of buffer
+ *Return: new string.
  */
-
 char *_memset(char *S, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i = 0;
+	while (i < n)
 	{
-		s[n - 1] = b;
-		n--;
+		*(s + i) = b;
+		i++;
 	}
 	return (s);
-}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,15 +6,15 @@
 * int prototypes() 
 */  
 
-int _putchar(char);                                        
+int _putchar(char c);            
 char *_memset(char *S, char b, unsigned int n);              
 char *_memcpy(char *dest, char *src, unsigned int n);
 char strchr(char *S, char c);
-unsigned int _strspn( char *S, char *accept);
+unsigned int _strspn( char *S, char accept);
 char *_strpbrk(char *S, char *accept);
 char *_strstr(char *haystack, char *needle);                
 void print_chessboard(char (*a)[8]);                         
 void print_diagsums(int *a, int size);                      
 void set_string(char **S, char *to);                         
 
-#endif /*MAIN_H*/   
+#endif /*MAIN.H*/
