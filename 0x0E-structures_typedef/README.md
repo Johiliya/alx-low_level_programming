64f48b5eeaabf2aa14f9088f65c2c20ec2d9cdbf
Johiliya <iliyajohnbobai@gmail.com>
2022-10-10 16:28:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+0. Poppy
+1. Write a function that initialize a variable of type struct dog
+1. Define a new type dog_t as a new name for the type struct dog.
+3. Write a function that creates a new dog.
+4. Write a function that frees dogs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: This is for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void int_dog(struct dog *d, char *name, float age, char *owner);
+void intt_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
+
