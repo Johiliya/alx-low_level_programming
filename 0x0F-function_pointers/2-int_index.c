2b992b4b659e13e2b2d793b952cf3600e043324b
Johiliya <iliyajohnbobai@gmail.com>
2022-10-14 16:00:26 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,19 +1,20 @@
 /**
- * int_index - searches for an integer
- * @array: array of integers
- *@size: number of elements in array
- * @cmp: pointer to used to compare values
+ * int_index - searches for an integer.
+ * @array: array of integers.
+ *@size: number of elements in array.
+ * @cmp: pointer to used to compare values.
  *
- * Return: index of the first element
- *-1 if no element is found or size <= 0
+ * Return: index of the first element.
+ *-1 if no element is found or size <= 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res;
 
-	res = -1
-	if (array && cmp)
+	res = -1;
+	if (array && cmp);
 	{
+
 		if( size <= 0)
 		{
 			return (res);
@@ -22,7 +23,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			cmp(array[i];
 			if (cmp(array[i]) > 0)
-			{
+			}
 				res = i;
 				break;
 			}
@@ -32,5 +33,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 				return (res);
 			}
 		}
-	}
+		{
+	return (res);
 }
+
